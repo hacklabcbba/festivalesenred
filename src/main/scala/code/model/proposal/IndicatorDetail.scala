5ea84822914eb3049087e0d48520e380a56b0347
Nataly Huanca <nataly.huanca@genso.com.bo>
2015-01-13 15:28:24 -0400

    change observations

--- src/main/scala/code/model/proposal/IndicatorDetail.scala
+++ src/main/scala/code/model/proposal/IndicatorDetail.scala
@@ -3,19 +3,19 @@ package model
 package proposal
 
 import net.liftweb.mongodb.record.{BsonMetaRecord, BsonRecord}
-import net.liftweb.record.field.{DoubleField, StringField}
+import net.liftweb.record.field.{DecimalField, StringField}
 
 class IndicatorDetail private () extends BsonRecord[IndicatorDetail] {
 
   override def meta = IndicatorDetail
 
   object description extends StringField(this, 500)
-  object inputValue extends DoubleField(this)
-  object inputValueFde extends DoubleField(this)
-  object outputValue extends DoubleField(this)
-  object outputValueFde extends DoubleField(this)
-  object percentValue extends DoubleField(this)
-  object percentValueFde extends DoubleField(this)
+  object inputValue extends DecimalField(this, 0)
+  object inputValueFde extends DecimalField(this, 0)
+  object outputValue extends DecimalField(this, 0)
+  object outputValueFde extends DecimalField(this, 0)
+  object percentValue extends DecimalField(this, 0)
+  object percentValueFde extends DecimalField(this, 0)
 
 }
 
