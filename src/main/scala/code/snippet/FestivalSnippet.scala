481d5b0cd50c5bd2b7725c0de88a7936e6e4f4b3
Juan Jose Olivera <juan@genso.com.bo>
2015-03-17 19:39:46 -0400

    snippethelper

--- src/main/scala/code/snippet/FestivalSnippet.scala
+++ src/main/scala/code/snippet/FestivalSnippet.scala
@@ -2,9 +2,10 @@ package code
 package snippet
 
 import code.config.Site
+import net.liftmodules.extras.SnippetHelper
 import net.liftweb.util.Helpers._
 
-object FestivalSnippet {
+object FestivalSnippet extends SnippetHelper {
 
   def edit = {
     "*" #> Site.festival.currentValue.map(festival => {
