3d3b060c04548a4447df5b0c9031552da04a6f6a
Juan Jose Olivera <juan@genso.com.bo>
2015-03-17 19:53:35 -0400

    Added screen

--- src/main/scala/code/snippet/FestivalSnippet.scala
+++ src/main/scala/code/snippet/FestivalSnippet.scala
@@ -3,14 +3,21 @@ package snippet
 
 import code.config.Site
 import net.liftmodules.extras.SnippetHelper
+import net.liftweb.common._
+import net.liftweb.http.S
+import net.liftweb.util.FieldContainer
 import net.liftweb.util.Helpers._
 
-object FestivalSnippet extends SnippetHelper {
+object FestivalSnippet extends BaseScreen {
 
-  def edit = {
-    "*" #> Site.festival.currentValue.map(festival => {
-      "data-name=form" #> festival.toForm(f => ())
-    })
+  addFields(() => new FieldContainer {def allFields = Site.festival.currentValue.map(_.fields()) openOr Nil})
+
+  def finish() {
+    Site.festival.currentValue.flatMap(s => tryo(s.save(true))) match {
+      case Empty => S.warning("Empty save")
+      case Failure(msg, _, _) => S.error(msg)
+      case Full(_) => S.notice("Festival saved")
+    }
   }
 
 }
